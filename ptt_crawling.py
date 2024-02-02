a66575fa5d073260b243647c7997ac0598d49590
w81015 <w81015@gmail.com>
2024-02-02 20:04:22 +0800

    first commit

---
+++ ptt_crawling.py
@@ -0,0 +1,101 @@
+# 爬網頁的程式
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import csv
+import time
+import json
+
+# 設定headers
+headers = {
+    'cookie': 'over18=1',
+    'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36'
+}
+
+# 爬取文章的詳細資訊
+def get_article_content(article_url):
+    response = requests.get(article_url, headers=headers)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    meta_values = soup.find_all('span', class_='article-meta-value')
+    if not meta_values:
+        return None
+
+    author, board, title, date = (meta.text for meta in meta_values)
+    content = soup.find(id="main-content").text
+    content = content.split(date)[-1].split('※ 發信站: 批踢踢實業坊(ptt.cc)')[0].split('\n', 1)[-1].rsplit('\n', 1)[0]
+
+    # 爬取留言
+    comments = []
+    push_tags = soup.find_all('div', class_='push')
+    for push_tag in push_tags:
+        push_type = push_tag.find('span', class_='push-tag').text.strip()
+        push_user = push_tag.find('span', class_='push-userid').text.strip()
+        push_content = push_tag.find('span', class_='push-content').text.strip()[2:]
+        push_ipdatetime = push_tag.find('span', class_='push-ipdatetime').text.strip()
+        
+        comment = {
+            'type': push_type.strip(),
+            'user': push_user,
+            'content': push_content,
+            'ipdatetime': push_ipdatetime
+        }
+        
+        comments.append(comment)
+
+    return {
+        'author': author,
+        'board': board,
+        'title': title,
+        'date': date,
+        'content': content,
+        'comments': comments,
+        'link': article_url
+    }
+
+# 所有文章連結
+def get_articles_list(url):
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    return ['https://www.ptt.cc' + link.a['href'] for link in soup.find_all('div', class_='title') if link.a]
+
+# 主函式: 爬取多頁，儲存結果
+def main(url, pages, csv_file_path):
+    with open(csv_file_path, 'w', newline='', encoding='utf_8_sig') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['作者', '看板', '標題', '日期', '內文', '推文數', '噓文數', '箭頭留言數', '連結', '留言資訊'])
+        
+    for _ in range(pages):
+        article_urls = get_articles_list(url)
+        for article_url in article_urls:
+            article_data = get_article_content(article_url)
+            if article_data:
+                # 初始化推文、噓文和箭頭留言的計數器
+                pushes = 0
+                boos = 0
+                arrows = 0
+                # 遍歷留言列表來計算推文、噓文和箭頭留言的數量
+                for comment in article_data['comments']:
+                    if comment['type'] == '推':
+                        pushes += 1
+                    elif comment['type'] == '噓':
+                        boos += 1
+                    elif comment['type'] == '→':
+                        arrows += 1
+                # 將留言資訊轉換成json
+                comments_json = json.dumps(article_data['comments'], ensure_ascii=False)
+                with open(csv_file_path, 'a', newline='', encoding='utf_8_sig') as csvfile:
+                    writer = csv.writer(csvfile)
+                    writer.writerow([article_data['author'], article_data['board'], article_data['title'], article_data['date'], article_data['content'], pushes, boos, arrows, article_data['link'], comments_json])
+            time.sleep(0.1)
+        response = requests.get(url, headers=headers)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        next_page = 'https://www.ptt.cc' + soup.find('a', string='‹ 上頁')['href']
+        url = next_page
+
+
+# 替換成要爬取的PTT板網址、頁數、要儲存的檔名
+if __name__ == "__main__":
+    url = 'https://www.ptt.cc/bbs/Gossiping/index.html'
+    pages = 1
+    csv_file_path = 'ptt_gossiping.csv'
+    main(url, pages, csv_file_path)
---
+++ ptt_data_processing.py
@@ -0,0 +1,69 @@
+import pandas as pd
+import json
+
+# 將抓取的資料轉換成df
+def info(filename):
+    all_info = pd.read_csv(filename)
+    return all_info
+
+
+# 將標題單獨存成一個column
+def title_processing(all_info):
+    all_info['category'] = all_info['標題'].apply(lambda x: x.split(']')[0])
+    all_info['category'] = all_info['category'].apply(lambda x: x.replace('[', ''))
+    all_info['標題'] = all_info['標題'].apply(lambda x: x.split(']')[1])  
+    return all_info
+
+
+# 將column轉換成英文，重新排序
+def column_name_processing(dll_info):
+    all_info = dll_info.rename(columns={ '作者': 'author','看板': 'board','標題': 'title','日期': 'date','內文': 'content',
+                    '推文數': 'push','噓文數': 'boo','箭頭留言數': 'arrow','連結': 'link','留言資訊': 'comment_info'
+                    })
+    
+    all_info = all_info[['board', 'author', 'category', 'title', 'date', 'content', 'push', 'boo', 'arrow', 'comment_info', 'link']]
+
+    return all_info  
+
+
+# 將所有留言取出，另外存成df
+def comment_processing(all_info):
+
+    # 從所有的資料集取出留言，存成df
+    all_comments = pd.DataFrame()
+    for i in all_info.index:
+        comments = json.loads(all_info.loc[i, 'comment_info'])
+        comments_df = pd.DataFrame(comments)
+        comments_df['source_index'] = i
+        all_comments = pd.concat([all_comments, comments_df], ignore_index=True)
+
+    # 重新命名留言的column，以免和原本的資料集搞混
+    all_comments.rename(columns={'user': 'commenter', 'content': 'comment'}, inplace=True)
+
+    # 將留言的ip和datetime分開
+    all_comments['date_time'] = all_comments['ipdatetime'].apply(lambda x: x[-11:])
+    all_comments['ip'] = all_comments['ipdatetime'].apply(lambda x: x[:-12])
+    all_comments.drop(['ipdatetime'], axis=1)
+
+    return all_comments
+
+
+# 將結果存成csv檔
+def save_to_csv(all_info, all_comments):
+    all_info.to_csv('all_info.csv', index=False, encoding='utf-8-sig')
+    all_comments.to_csv('all_comments.csv', index=False, encoding='utf-8-sig')
+
+
+def main():
+    filename = 'ptt_gossiping.csv'
+    all_info = info(filename)
+    all_info = title_processing(all_info)
+    all_info = column_name_processing(all_info)
+    all_comments = comment_processing(all_info)
+    save_to_csv(all_info, all_comments)
+
+    print('Data processing complete!')
+
+
+if __name__ == "__main__":
+    main()
